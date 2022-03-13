f99cd54fbd655e680403a6a2a71bfdb46883e877
Lasse Lueders <lasselueders@gmx.de>
2022-03-13 10:40:07 +0100

    Test navigation

--- src/components/Navigation.spec.js
+++ src/components/Navigation.spec.js
@@ -1,10 +1,26 @@
-import {render, screen} from '@testing-library/react'
-  import Navigation from './Navigation'
-    
-  describe('Navigation', () => {
-      it('renders', () => {
-          render(<Navigation />)
-          expect(screen.getByText('Navigation')).toBeInTheDocument()
-      })
-  })
-            
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import Navigation from './Navigation.js';
+
+describe('Navigation', () => {
+  it('renders navigation with two links and two buttons with img', () => {
+    render(
+      <MemoryRouter>
+        <Navigation />
+      </MemoryRouter>
+    );
+    // screen.getAllByRole('');
+
+    const navbar = screen.getByRole('navigation');
+    expect(navbar).toBeInTheDocument();
+
+    const links = screen.getAllByRole('link');
+    expect(links).toHaveLength(2);
+
+    const buttons = screen.getAllByRole('button');
+    expect(buttons).toHaveLength(2);
+
+    const images = screen.getAllByRole('img');
+    expect(images).toHaveLength(2);
+  });
+});
