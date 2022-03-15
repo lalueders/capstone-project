cffe0b5a80bf345f8d7a82d3497a65303a4a4fe6
Lasse Lueders <lasselueders@gmx.de>
2022-03-15 14:36:53 +0100

    Test component AddButton

--- src/components/AddButton.spec.js
+++ src/components/AddButton.spec.js
@@ -1,10 +1,19 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import AddButton from './AddButton';
 
 describe('AddButton', () => {
   it('renders a button with an image as content', () => {
-    render(<AddButton />);
+    render(
+      <MemoryRouter>
+        <AddButton />
+      </MemoryRouter>
+    );
 
-    expect(screen.getByText('AddButton')).toBeInTheDocument();
+    const button = screen.getByRole('img', { name: /create note/i });
+    const icon = screen.getByRole('img', { name: /create note/i });
+
+    expect(button).toBeInTheDocument();
+    expect(icon).toBeInTheDocument();
   });
 });
