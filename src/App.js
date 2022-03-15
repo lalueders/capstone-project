8bbf5c026dbff9dda2911d4fbe0ee9edd89a4a27
Lasse Lueders <lasselueders@gmx.de>
2022-03-15 15:07:45 +0100

    Comment out test data for deployment

--- src/App.js
+++ src/App.js
@@ -6,41 +6,41 @@ import FormPage from './pages/FormPage.js';
 import Navigation from './components/Navigation.js';
 
 function App() {
-  // const [notes, setNotes] = useState([]);
-  const [notes, setNotes] = useState([
-    {
-      date: '2022-07-03',
-      title: 'Capstone Projekt gestartet',
-      text: 'Es geht los! Mal schauen, was m,an in vier Wochen so schafft...',
-      location: 'Hamburg, Eimsbüttel',
-      categories: ['job', 'carrer'],
-    },
+  // const [notes, setNotes] = useState([
+  //   {
+  //     date: '2022-07-03',
+  //     title: 'Capstone Projekt gestartet',
+  //     text: 'Es geht los! Mal schauen, was m,an in vier Wochen so schafft...',
+  //     location: 'Hamburg, Eimsbüttel',
+  //     categories: ['job', 'carrer'],
+  //   },
 
-    {
-      date: '2022-01-10',
-      title: 'Das Bootcamp geht los!',
-      text: 'Man sagt da kommt ganz schön viel Arbeit auf mich zu...',
-      location: 'Hamburg, Eimsbüttel',
-      categories: ['job', 'carrer'],
-    },
+  //   {
+  //     date: '2022-01-10',
+  //     title: 'Das Bootcamp geht los!',
+  //     text: 'Man sagt da kommt ganz schön viel Arbeit auf mich zu...',
+  //     location: 'Hamburg, Eimsbüttel',
+  //     categories: ['job', 'carrer'],
+  //   },
 
-    {
-      date: '2022-1-03',
-      title: 'Family zu Besuch in HH',
-      text: 'Die Karlsruher haben es auch mal in den Norden geschafft! Emma ist echt groß geworden und Lisa wachsen immernoch keine Haare',
-      location: 'Hamburg, Rotherbaum',
-      categories: ['family', 'vacation'],
-    },
+  //   {
+  //     date: '2022-1-03',
+  //     title: 'Family zu Besuch in HH',
+  //     text: 'Die Karlsruher haben es auch mal in den Norden geschafft! Emma ist echt groß geworden und Lisa wachsen immernoch keine Haare',
+  //     location: 'Hamburg, Rotherbaum',
+  //     categories: ['family', 'vacation'],
+  //   },
 
-    {
-      date: '2022-11-15',
-      title: 'Leyla ist da!',
-      text: 'Na endlich! Happy Welcome to the World Leyla Hayal!!! 2.25 Uhr, 3500g, 53cm',
-      location: 'Hamburg, Eimsbüttel',
-      categories: ['family'],
-    },
-  ]);
+  //   {
+  //     date: '2022-11-15',
+  //     title: 'Leyla ist da!',
+  //     text: 'Na endlich! Happy Welcome to the World Leyla Hayal!!! 2.25 Uhr, 3500g, 53cm',
+  //     location: 'Hamburg, Eimsbüttel',
+  //     categories: ['family'],
+  //   },
+  // ]);
 
+  const [notes, setNotes] = useState([]);
   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
 
   const showFormSubmitMessage = () => {
