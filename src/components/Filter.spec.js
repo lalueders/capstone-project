77f3c125c881df1a49de654ab4828366c8151357
Lasse Lueders <lasselueders@gmx.de>
2022-03-27 11:09:24 +0200

    update filter test

--- src/components/Filter.spec.js
+++ src/components/Filter.spec.js
@@ -2,16 +2,15 @@ import { render, screen } from '@testing-library/react';
 import Filter from './Filter';
 
 describe('Filter', () => {
-  it('renders five buttons', () => {
-    render(<Filter />);
+  it('renders four buttons', () => {
+    const categories = ['family', 'friends', 'vacation', 'others'];
+    render(<Filter categories={categories} />);
 
-    const button1 = screen.getByRole('button', { name: /all/i });
     const button2 = screen.getByRole('button', { name: /family/i });
     const button3 = screen.getByRole('button', { name: /friends/i });
     const button4 = screen.getByRole('button', { name: /vacation/i });
     const button5 = screen.getByRole('button', { name: /others/i });
 
-    expect(button1).toBeInTheDocument();
     expect(button2).toBeInTheDocument();
     expect(button3).toBeInTheDocument();
     expect(button4).toBeInTheDocument();
